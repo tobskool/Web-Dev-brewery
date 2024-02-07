5a6f2c8de6ad5102546373b9e43b6459ff88b295
Tobskool <tfayemi9@gmail.com>
2024-02-07 11:18:03 +0000

    added parameter encoding example to read from text file using file system (fs) native module. Added example and tested both method/ lines of code work

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -8,8 +8,14 @@ const fs = require("fs")
 //     console.log('The file has been saved!');
 //   } ); 
 
-fs.readFile('message2.txt', (err, data) => { //can also put the extra parameter of encoding type (UTF8)
+fs.readFile('message2.txt', (err, data) => { // (can also put the extra parameter of encoding type (UTF8) see below...)
     if (err) throw err;
     const text = data.toString();
     console.log(text);
 });
+
+// fs.readFile('message2.txt', 'utf-8', (err, data) => { //can also put the extra parameter of encoding type (UTF8)
+//     if (err) throw err;
+//     const text = data.toString();
+//     console.log(text);
+// });
